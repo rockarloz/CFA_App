6ba24a0028c4dafccc6c5f2069f71ec8f41c1750
Carlos Castellanos <rockarlos@me.com>
2015-01-30 23:40:04 -0600

    Initial Commit

---
+++ CFA/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CFA
+//
+//  Created by Carlos Castellanos on 30/01/15.
+//  Copyright (c) 2015 Carlos Castellanos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CFA/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CFA
+//
+//  Created by Carlos Castellanos on 30/01/15.
+//  Copyright (c) 2015 Carlos Castellanos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
